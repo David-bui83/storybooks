13ef62e40e667edcbafb3b5d8bed5aacb0d073d0
Mona Lisa <david.t.bui83@gmail.com>
2020-05-11 17:09:51 -0700

    handled auth

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const passport = require('passport');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Passport config
+require('./config/passport')(passport);
 
+// Load Routes
+const auth = require('./routes/auth');
 
 // Index route 
 app.get('/', (req, res) => {
   res.send('It works!');
 });
+
+// Use Routes
+app.use('/auth', auth);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
   console.log(`Server started on port ${PORT}`);
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://storybooks:storybooks20@ds153729.mlab.com:53729/storybooks-dev',
+  googleClientID: '1036091207518-sc2chkj27t652iq1eboku40ven74hprv.apps.googleusercontent.com',
+  googleClientSecret: 'qkoPB3BeQKfAv8uuIvFI8swl'
+}
